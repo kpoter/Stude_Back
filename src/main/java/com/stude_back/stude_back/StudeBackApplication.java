40059ee689a781c2135788aba64849946ebdcac0
kpoter <kpoter@naver.com>
2024-06-04 16:49:41 +0900

    init commmit

---
+++ src/main/java/com/stude_back/stude_back/StudeBackApplication.java
@@ -0,0 +1,13 @@
+package com.stude_back.stude_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudeBackApplication {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(StudeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stude_back/stude_back/StudeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stude_back.stude_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
